abd81e58d055381a359a531a3d5a5a7847dd67b2
Steve Guidetti <stevotvr@gmail.com>
2017-12-01 13:15:33 -0800

    Add day 1 solutions

---
+++ day01/part1.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.readFile(__dirname + '/input.txt', 'utf8', function (err, data) {
+    var sum = 0;
+    for (var i = 0; i < data.length; i++) {
+        var next = (i === data.length - 1) ? 0 : i + 1;
+        if (data.charAt(i) === data.charAt(next)) {
+            sum += parseInt(data.charAt(i));
+        }
+    }
+
+    console.log(sum);
+});
