e575c911f3a289a8c73e27b7a02251aa16b73f09
Steve Guidetti <stevotvr@gmail.com>
2017-12-04 21:18:26 -0800

    Add day 5 solutions

---
+++ day05/part1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
+    data = data.trim();
+    data = data.split('\n').map(x => parseInt(x));
+    var count = 0;
+    var offset = 0;
+    while(offset >= 0 && offset < data.length) {
+        offset += data[offset]++;
+        count++;
+    }
+
+    console.log(count);
+});
---
+++ day05/part2.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
+    data = data.trim();
+    data = data.split('\n').map(x => parseInt(x));
+    var count = 0;
+    var offset = 0;
+    while(offset >= 0 && offset < data.length) {
+        var toffset = offset;
+        offset += data[offset];
+        data[toffset] += data[toffset] >= 3 ? -1 : 1;
+        count++;
+    }
+
+    console.log(count);
+});
