798e75445fc0a727cdb4238fd9ae8613beb8e69a
Steve Guidetti <stevotvr@gmail.com>
2017-12-05 21:38:02 -0800

    Add day 6 solutions

---
+++ day06/part1.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
+    data = data.trim();
+    const banks = data.split(/\s+/).map(Number);
+    const states = {};
+    states[banks.join('|')] = true;
+    var cycles = 0;
+    while(true) {
+        redistribute(banks);
+        cycles++;
+        var hash = banks.join('|');
+        if(states[hash]) {
+            break;
+        }
+        states[hash] = true;
+    }
+
+    console.log(cycles);
+});
+
+function redistribute(banks) {
+    var idx = getLargest(banks);
+    var value = banks[idx];
+    banks[idx] = 0;
+    while(value) {
+        idx = (idx + 1) % banks.length;
+        banks[idx]++;
+        value--;
+    }
+}
+
+function getLargest(banks) {
+    var largest = 0;
+    var key = 0;
+    for(var i = 0; i < banks.length; i++) {
+        if(banks[i] > largest) {
+            largest = banks[i];
+            key = i;
+        }
+    }
+    return key;
+}
