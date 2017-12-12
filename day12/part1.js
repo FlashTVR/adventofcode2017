b61f91cb4cfcfbedbe99348c88c09b219d8c1abb
Steve Guidetti <stevotvr@gmail.com>
2017-12-12 12:01:25 -0800

    Add day 12 solutions

---
+++ day12/part1.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
+    data = data.trim();
+    const nodes = new Map();
+    for(const line of data.split('\n')) {
+        var [node, recipients] = line.split('<->');
+        node = Number(node);
+        recipients = recipients.split(',').map(Number);
+        if(!nodes.has(node)) {
+            nodes.set(node, new Set());
+        }
+        for(const recipient of recipients) {
+            if(!nodes.has(recipient)) {
+                nodes.set(recipient, new Set());
+            }
+            nodes.get(node).add(recipient);
+            nodes.get(recipient).add(node);
+        }
+    }
+
+    const visited = new Set();
+    const stack = [0];
+    while(stack.length) {
+        const current = stack.pop();
+        visited.add(current);
+        for(const recipient of nodes.get(current)) {
+            if(visited.has(recipient)) {
+                continue;
+            }
+            stack.push(recipient);
+        }
+    }
+
+    console.log(visited.size);
+});
---
+++ day12/part2.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
+    data = data.trim();
+    const nodes = new Map();
+    for(const line of data.split('\n')) {
+        var [node, recipients] = line.split('<->');
+        node = Number(node);
+        recipients = recipients.split(',').map(Number);
+        if(!nodes.has(node)) {
+            nodes.set(node, new Set());
+        }
+        for(const recipient of recipients) {
+            if(!nodes.has(recipient)) {
+                nodes.set(recipient, new Set());
+            }
+            nodes.get(node).add(recipient);
+            nodes.get(recipient).add(node);
+        }
+    }
+
+    let groups = 0;
+    const stack = [];
+    while(nodes.size) {
+        stack.push(nodes.keys().next().value);
+        while(stack.length) {
+            const current = stack.pop();
+            const recipients = nodes.get(current);
+            if(!recipients) {
+                continue;
+            }
+            for(const recipient of recipients) {
+                stack.push(recipient);
+            }
+            nodes.delete(current);
+        }
+        groups++;
+    }
+
+    console.log(groups);
+});
