ba4b6f188e73351eaca52b87f48b4e01420b70b3
Steve Guidetti <stevotvr@gmail.com>
2017-12-16 21:33:36 -0800

    Remove unnecessary variable

--- day17/part2.js
+++ day17/part2.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
     const steps = Number(data);
-    let pos = 0, len = 1, value = 0;
+    let pos = 0, value = 0;
     for(let i = 1; i <= 50000000; i++) {
-        pos = ((pos + steps) % len++) + 1;
+        pos = ((pos + steps) % i) + 1;
         if(pos === 1) {
             value = i;
         }
