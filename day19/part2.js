edbfc70b342d7d0bb60a05bac0efebae205032ea
Steve Guidetti <stevotvr@gmail.com>
2017-12-19 12:47:13 -0800

    Add day 19 solutions

---
+++ day19/part2.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
+    const grid = data.split('\n').map((l) => [...l.replace('\r', '')]);
+    const pos = { r: 0, c: grid[0].indexOf('|') }, dir = { r: 1, c: 0 }, path = [];
+    let count = 0;
+    while(true) {
+        if(grid[pos.r][pos.c] === '+') {
+            if(dir.r === 0) {
+                dir.c = 0;
+                dir.r = (grid[pos.r - 1] && grid[pos.r - 1][pos.c] && grid[pos.r - 1][pos.c] !== ' ') ? -1 : 1;
+            } else {
+                dir.r = 0;
+                dir.c = (grid[pos.r][pos.c - 1] && grid[pos.r][pos.c - 1] !== ' ') ? -1 : 1;
+            }
+        } else if(grid[pos.r][pos.c].match(/[A-Z]/)) {
+            path.push(grid[pos.r][pos.c]);
+        }
+        pos.r += dir.r;
+        pos.c += dir.c;
+        count++;
+        if(!grid[pos.r] || !grid[pos.r][pos.c] || grid[pos.r][pos.c] === ' ') {
+            break;
+        }
+    }
+
+    console.log(count);
+});
