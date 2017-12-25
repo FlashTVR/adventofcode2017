6e4a7c61556e749d2577a45019716a3d8acd59dd
Steve Guidetti <stevotvr@gmail.com>
2017-12-25 10:45:46 -0800

    Generalize solution using data read from the input file

--- day25/part1.js
+++ day25/part1.js
@@ -1,75 +1,33 @@
 const fs = require('fs');
 
 fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
-    const checkInt = Number(data.split('\n')[1].split(' ')[5]);
+    data = data.trim().replace(/\r/g, '').split('\n\n');
+    const init = data.shift().split('\n');
 
-    let state = 'A', pos = 0;
+    const ops = {};
+    while(data.length) {
+        const block = data.shift().split('\n');
+        const blockState = block.shift().match(/In state ([A-Z]):/)[1];
+        ops[blockState] = [];
+        while(block.length) {
+            const idx = Number(block.shift().trim().match(/If the current value is ([0-9]+):/)[1]);
+            const write = Number(block.shift().trim().match(/- Write the value ([0-9]+)\./)[1]);
+            const move = (block.shift().trim().match(/- Move one slot to the (right|left)\./)[1] === 'right') ? 1 : -1;
+            const next = block.shift().trim().match(/- Continue with state ([A-Z])\./)[1];
+            ops[blockState][idx] = ((write, move, next) => {
+                return (tape, pos) => {
+                    tape.set(pos, write);
+                    return [pos + move, next];
+                };
+            })(write, move, next);
+        }
+    }
+
     const tape = new Map();
-    const ops = {
-        A: [
-            () => {
-                tape.set(pos++, 1);
-                state = 'B';
-            },
-            () => {
-                tape.set(pos--, 0);
-                state = 'C';
-            },
-        ],
-        B: [
-            () => {
-                tape.set(pos--, 1);
-                state = 'A';
-            },
-            () => {
-                tape.set(pos--, 1);
-                state = 'D';
-            },
-        ],
-        C: [
-            () => {
-                tape.set(pos++, 1);
-                state = 'D';
-            },
-            () => {
-                tape.set(pos++, 0);
-                state = 'C';
-            },
-        ],
-        D: [
-            () => {
-                tape.set(pos--, 0);
-                state = 'B';
-            },
-            () => {
-                tape.set(pos++, 0);
-                state = 'E';
-            },
-        ],
-        E: [
-            () => {
-                tape.set(pos++, 1);
-                state = 'C';
-            },
-            () => {
-                tape.set(pos--, 1);
-                state = 'F';
-            },
-        ],
-        F: [
-            () => {
-                tape.set(pos--, 1);
-                state = 'E';
-            },
-            () => {
-                tape.set(pos++, 1);
-                state = 'A';
-            },
-        ],
-    };
-
-    for(let i = 0; i < checkInt; i++) {
-        ops[state][tape.get(pos) || 0]();
+    let pos = 0, state = init[0].match(/Begin in state ([A-Z])\./)[1];
+    const check = Number(init[1].match(/Perform a diagnostic checksum after ([0-9]+) steps\./)[1]);
+    for(let i = 0; i < check; i++) {
+        [pos, state] = ops[state][tape.get(pos) || 0](tape, pos);
     }
 
     console.log([...tape.values()].reduce((a, b) => a + b));
